1a9b8dbb1502631e1df022d112d74f9c558ee4c5
Alander Machado <alandermm@hotmail.com>
2017-11-23 19:40:52 -0200

    ExemploDelegate

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExemploDelegate
+{
+    class Program
+    {
+        delegate void Cortador(string queijo);
+
+        static void Main(string[] args)
+        {
+            Cortador vera = new Cortador(Data);
+            vera("Olá, eu sou um delegate");
+            Cortador filha = new Cortador(DataCompleta);
+            
+            //Data("Olá ");
+            //Console.WriteLine(DataCompleta("Alander"));
+        }
+
+        static void Data(string texto){
+            Console.WriteLine(texto + " Hoje é " + DateTime.Now.ToShortDateString());
+        }
+
+        static string DataCompleta(string nome){
+            return "Olá " + nome + " Hoje é " + DateTime.Now.ToLongDateString();
+        }
+    }
+}
